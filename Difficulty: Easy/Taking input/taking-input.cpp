f8715b19efc7e4345713cd0d90378f3d9d8b5a4d
Aaryan Raj <132547352+aaryan77raj@users.noreply.github.com>
2024-07-03 06:49:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Taking input/taking-input.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    void inputData() {
+        int a;
+        cin >> a;
+        string b;
+        cin >> b;
+        cout << a << " " << b << endl;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        Solution obj;
+
+        obj.inputData();
+    }
+}
+
+// } Driver Code Ends
